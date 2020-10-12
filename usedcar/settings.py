015389feee51f4a448be598ffb2c927266dd4e35
Chris Li <lihongyu0807@icloud.com>
2020-10-12 19:35:40 +0800

    update scrapy setting
    try to fix 503 error

--- usedcar/settings.py
+++ usedcar/settings.py
@@ -24,7 +24,7 @@ LOG_LEVEL = 'INFO'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -73,16 +73,16 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_ENABLED = True
+#The initial download delay
+AUTOTHROTTLE_START_DELAY = 1
+#The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 60
+#The average number of requests Scrapy should be sending in parallel to
+#each remote server
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
+#Enable showing throttling stats for every response received:
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
