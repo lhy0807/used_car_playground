a21d7bfc4296e65866f2e0cf484c868eba4544e3
Chris Li <lihongyu0807@icloud.com>
2020-10-25 20:20:50 +0800

    another webapp solution using Flask

---
+++ gpdapp/main.py
@@ -0,0 +1,51 @@
+from random import random
+
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import pandas as pd
+import json
+import numpy as np
+from io import BytesIO
+import base64
+
+from atlas_db import atlas
+import pymongo
+
+from flask import Flask, redirect, url_for, render_template, request
+
+print("loading geojson file...")
+ny = gpd.read_file("geojson/ny.geojson")
+ny = ny.set_index('ZIP_CODE')
+print("finish loading geojson file...")
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  fig, ax = plt.subplots(1, 1)
+  ny.plot(column="Points", missing_kwds={'color': 'lightgrey'}, ax=ax, legend=True)
+  buf = BytesIO()
+  fig.savefig(buf, format="png")
+  data = base64.b64encode(buf.getbuffer()).decode("ascii")
+  return render_template('index.html', fig=data)
+
+@app.route("/model", methods=['POST'])
+def model():
+  code = request.form['code']
+  global ny
+  file_name = "geojson/{}.json".format(code)
+  with open(file_name, 'r') as f:
+    data = json.load(f)
+    avg_points = data[0]
+    quantity = data[1]
+    for i in avg_points:
+      ny.at[i, 'Points'] = avg_points[i]
+
+    for i in quantity:
+      ny.at[i, 'Quantity'] = quantity[i]
+  return redirect(url_for('index'))
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0",port="5100")
