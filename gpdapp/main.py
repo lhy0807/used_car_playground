c86df072f89eece43e5fddae73ed56a059afbf0f
Chris Li <lihongyu0807@icloud.com>
2020-10-29 23:32:20 +0800

    add map ajax, loading much faster

--- gpdapp/main.py
+++ gpdapp/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import pandas as pd
 import json
+import time
 import numpy as np
 from io import BytesIO
 import base64
@@ -13,7 +14,8 @@ import base64
 from atlas_db import atlas
 import pymongo
 
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, make_response
+from concurrent.futures import ThreadPoolExecutor
 
 print("loading geojson file...")
 ny = gpd.read_file("geojson/ny.geojson")
@@ -26,18 +28,21 @@ usedcar = db.cargurus
 modelcode = db.modelcode
 print("mongodb connected!")
 
+
+
 app = Flask(__name__)
 
-def model_num():
+def model_num_fn():
   print("get model number")
   return len(usedcar.distinct("model"))
 
-def model_table():
+def model_table_fn():
   print("get models")
   return list(modelcode.find({"code":{"$in":usedcar.distinct("model")}}))
 
-def make_pie():
+def make_pie_fn():
 #pie chart for each car make
+  print("get pie chart data")
   make = modelcode.find({"code":{"$in":usedcar.distinct("model")} }).distinct("make")
   num = []
   for i in make:
@@ -48,37 +53,63 @@ def make_pie():
     porp.append(float(np.round((i/sum(num)*100), 2)))
   return [make, porp]
 
+#init
+model_num = 0
+model_table = list()
+pie_name = list()
+pie_porp = list()
+#start threads
+with ThreadPoolExecutor() as executor:
+  model_num_thread = executor.submit(model_num_fn)
+  model_table_thread = executor.submit(model_table_fn)
+  make_pie_thread = executor.submit(make_pie_fn)
+
 @app.route("/")
 def index(df = ny):
+
+  model_num = model_num_thread.result()
+  model_table = model_table_thread.result()
+  pie_name = make_pie_thread.result()[0]
+  pie_porp = make_pie_thread.result()[1]
+
+  return render_template('index.html', model_num=model_num, model_table=model_table,
+   pie_name=json.dumps(pie_name), pie_porp=json.dumps(pie_porp), pie_name_no_json=pie_name )
+
+@app.route("/model", methods=['POST'])
+def model():
+  code = request.form['code']
+  resp = make_response(redirect(url_for('index')))
+  resp.set_cookie('map_code',code)
+  return resp
+
+@app.route("/ajax/map")
+def ajax(df = ny):
+  code = request.cookies.get("map_code")
+  if code != "":
+    df = ny.copy()
+    file_name = "geojson/{}.json".format(code)
+    with open(file_name, 'r') as f:
+      data = json.load(f)
+      avg_points = data[0]
+      quantity = data[1]
+      for i in avg_points:
+        df.at[i, 'Points'] = avg_points[i]
+
+      for i in quantity:
+        df.at[i, 'Quantity'] = quantity[i]
+
   fig, ax = plt.subplots(1, 1)
   df.plot(column="Points", missing_kwds={'color': 'lightgrey'}, ax=ax, legend=True)
 
   # adding title to the plot
   # TODO: get selected form input and put in plot
 
-  ax.set_title("United States Used Car Data by _type_")
+  ax.set_title("New York State Used Car Data by _type_")
 
   buf = BytesIO()
   fig.savefig(buf, format="png")
   data = base64.b64encode(buf.getbuffer()).decode("ascii")
-  return render_template('index.html', fig=data, model_num=model_num(), model_table=model_table(),
-   pie_name=json.dumps(make_pie()[0]), pie_porp=json.dumps(make_pie()[1]), pie_name_no_json=make_pie()[0] )
-
-@app.route("/model", methods=['POST'])
-def model():
-  code = request.form['code']
-  df = ny.copy()
-  file_name = "geojson/{}.json".format(code)
-  with open(file_name, 'r') as f:
-    data = json.load(f)
-    avg_points = data[0]
-    quantity = data[1]
-    for i in avg_points:
-      df.at[i, 'Points'] = avg_points[i]
-
-    for i in quantity:
-      df.at[i, 'Quantity'] = quantity[i]
-  return index(df=df)
+  return data
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port="5100")
