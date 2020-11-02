957614ef732962d62d4a9b9f65c487ba2650e8ba
Chris Li <lihongyu0807@icloud.com>
2020-11-03 01:37:54 +0800

    add multiple models selection

--- gpdapp/main.py
+++ gpdapp/main.py
@@ -16,6 +16,8 @@ import pymongo
 
 from flask import Flask, redirect, url_for, render_template, request, make_response
 from concurrent.futures import ThreadPoolExecutor
+from flask_wtf.csrf import CSRFProtect
+
 
 print("loading geojson file...")
 ny = gpd.read_file("geojson/ny.geojson")
@@ -31,6 +33,7 @@ print("mongodb connected!")
 
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 def model_num_fn():
   print("get model number")
@@ -75,6 +78,7 @@ def index(df = ny):
   return render_template('index.html', model_num=model_num, model_table=model_table,
    pie_name=json.dumps(pie_name), pie_porp=json.dumps(pie_porp), pie_name_no_json=pie_name )
 
+@csrf.exempt
 @app.route("/model", methods=['POST'])
 def model():
   code = request.form['code']
@@ -84,26 +88,71 @@ def model():
 
 @app.route("/ajax/map")
 def ajax(df = ny):
-  code = request.cookies.get("map_code")
-  if code != "" and code is not None:
+  #TODO:
+  #Modify the algo here
+  codes = request.cookies.get("map_code")
+  if codes != "" and codes is not None:
+    codes = json.loads(codes)
     df = ny.copy()
-    file_name = "geojson/{}.json".format(code)
-    with open(file_name, 'r') as f:
-      data = json.load(f)
-      avg_points = data[0]
-      quantity = data[1]
-      for i in avg_points:
-        df.at[i, 'Points'] = avg_points[i]
+    avg_points = {}
+    quantity = {}
+    for code in codes:
+      file_name = "geojson/{}.json".format(code)
+      with open(file_name, 'r') as f:
+        data = json.load(f)
+        #sum up avg_points
+        for i in data[0]:
+          if i in avg_points:
+            avg_points[i] = avg_points[i] + data[0][i]
+          else:
+            avg_points[i] = data[0][i]
+        
+        #sum up quantity
+        for i in data[1]:
+          if i in quantity:
+            quantity[i] = quantity[i] + data[1][i]
+          else:
+            quantity[i] = data[1][i]
 
-      for i in quantity:
-        df.at[i, 'Quantity'] = quantity[i]
+  #calculate avg of avg_points and quantity
+  max_points = 0
+  min_points = 9
+  max_quantity = 0
+  min_quantity = 9
+  for i in avg_points:
+    avg_points[i] = avg_points[i] / len(codes)
+    if avg_points[i] > max_points:
+      max_points = avg_points[i]
+    if avg_points[i] < min_points:
+      min_points = avg_points[i]
+
+  for i in quantity:
+    quantity[i] = quantity[i] / len(codes)
+    if quantity[i] > max_quantity:
+      max_quantity = quantity[i]
+    if quantity[i] < min_quantity:
+      min_quantity = quantity[i]
+  #normalization again
+  for i in avg_points:
+    avg_points[i] = (avg_points[i]-min_points)/(max_points-min_points)
+  for i in quantity:
+    quantity[i] = (quantity[i]-min_quantity)/(max_quantity-min_quantity)
+
+  for i in avg_points:
+    df.at[i, 'Points'] = avg_points[i]
+
+  for i in quantity:
+    df.at[i, 'Quantity'] = quantity[i]
 
   fig, ax = plt.subplots(1, 1)
   df.plot(column="Points", missing_kwds={'color': 'lightgrey'}, ax=ax, legend=True)
 
-  if code != "" and code is not None:
-    info = modelcode.find({"code":code})[0]
-    ax.set_title("New York State {} {} {}".format(info['year'],info['make'],info['model']))
+  if codes != [] and codes is not None:
+    if len(codes) == 1:
+      info = modelcode.find({"code":code})[0]
+      ax.set_title("New York State {} {} {}".format(info['year'],info['make'],info['model']))
+    else:
+      ax.set_title("Multiple models in New York State")
   else:
     ax.set_title("New York State Map")
   buf = BytesIO()
